7948b1af77eb3faa3cc7fba680a308f65d6cadc8
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-12-19 15:26:08 +0800

    chore: init

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by zhangzhihui on 2024/12/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
